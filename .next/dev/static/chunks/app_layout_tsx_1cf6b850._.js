7f9c4827580fadfe6c0cc07e690b6c339a09b264
OggyYggo <baliling.jabezcyrus@cec.edu.ph>
2026-03-04 11:27:13 +0800

    add gitignore

--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/styles_globals-tailwind_postcss_a047ed46.css"
-],
-    source: "dynamic"
-});
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"DZKLl+NLNIxtyoLoUlxVk7KMVXbM+436+I63I3O7ghM=\"\n}"
