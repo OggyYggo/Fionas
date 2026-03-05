05b7728915d51800867bcc1729af4e0d822795dc
OggyYggo <baliling.jabezcyrus@cec.edu.ph>
2026-03-06 03:33:13 +0800

    Add deployment timestamp to test Vercel updates

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,16 @@ import CTA from '@/components/CTA'
 import Footer from '@/components/Footer'
 
 export default function Home() {
+  const deployTime = new Date().toLocaleString();
+  
   return (
     <>
       <Header />
       <Hero />
+      {/* Deployment test - remove this line after confirming deployment works */}
+      <div className="text-center py-2 bg-blue-100 text-blue-800 text-sm">
+        Last deployed: {deployTime}
+      </div>
       <ToursSection />
       <DestinationsSection />
       <CTA />
