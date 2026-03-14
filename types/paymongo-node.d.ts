4a926ba450470038db79c1fbf84413af4046755a
OggyYggo <baliling.jabezcyrus@cec.edu.ph>
2026-03-14 23:00:39 +0800

    Fix TypeScript error - add type declaration for paymongo-node module

---
+++ types/paymongo-node.d.ts
@@ -0,0 +1,10 @@
+declare module 'paymongo-node' {
+  class Paymongo {
+    constructor(secretKey: string);
+    
+    // Add any methods you use from Paymongo here
+    // For now, we'll use a basic declaration
+  }
+  
+  export default Paymongo;
+}
